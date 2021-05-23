877e4aea39d14935389abc014fd5014e571db1e2
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2021-05-23 18:55:50 -0400

    Update ClientSetup.js

--- _test/ClientSetup.js
+++ _test/ClientSetup.js
@@ -2,4 +2,4 @@ let { Client } = require("../index");
 
 let client = new Client("<token>");
 
-client.startGateway("<token>") // token can go here or in the client constructor.
+client.startGateway("<token>"); // token can go here or in the client constructor.
