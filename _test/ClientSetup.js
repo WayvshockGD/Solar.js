846ea4b585293573efe5f6013532d8ff22d69918
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2021-05-23 18:20:01 -0400

    added new method in example

--- _test/ClientSetup.js
+++ _test/ClientSetup.js
@@ -1,3 +1,5 @@
 let { Client } = require("../index");
 
-let client = new Client();
+let client = new Client("<token>");
+
+client.startGateway("<token>") // token can go here or in the client constructor.
