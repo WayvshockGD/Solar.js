bd9a256c2d1f4ed2f4d9162909133d2d49375f6b
WayvshockGD <61085765+WayvshockGD@users.noreply.github.com>
2021-05-25 11:31:16 -0400

    Update EmbedSetup.js

--- _test/EmbedSetup.js
+++ _test/EmbedSetup.js
@@ -4,3 +4,6 @@ let embed = new EmbedBuilder()
             .setTitle("t")
             .setDescription("h")
             .setColor("")
+            .setFields([
+                  {name: "test", value: "words-here", inline: true}
+             ])
